a4132271f6739ca789d2320abef56478b2a337f4
akagupta9 <43808980+akagupta9@users.noreply.github.com>
2020-05-19 15:05:43 +0530

    Add files via upload

---
+++ src/test/java/com/automation/testcases/RegisterTests.java
@@ -0,0 +1,20 @@
+package com.automation.webautomation;
+
+import org.testng.annotations.Test;
+
+public class RegisterTests {
+	@Test
+	public void registerTest() {
+		System.out.println("Register Test 1");
+	}
+
+	@Test
+	public void registerTest2() {
+		System.out.println("Register Test 2");
+	}
+
+	@Test
+	public void registerTest3() {
+		System.out.println("Register Test 3");
+	}
+}
